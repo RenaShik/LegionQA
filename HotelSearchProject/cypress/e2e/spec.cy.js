f5fd437f0dbbc01c4c35413694414f91dfe267e2
RenaShik <rena.shikher@gmail.com>
2023-03-19 21:04:06 -0400

    Revert "first commit on hotel search site"
    This reverts commit 9089a3223f37ac5846f1193b53ce6751e252ea96.

--- HotelSearchProject/cypress/e2e/spec.cy.js
+++ HotelSearchProject/cypress/e2e/spec.cy.js
@@ -1,10 +1,10 @@
 ///<reference types = "Cypress"/>
 describe('empty spec', () => {
-  it('test cypress io examples page', () => {
-    cy.visit('https://example.cypress.io')
-    cy.url().should('eq', 'https://example.cypress.io/')
-  })
-
+    it('test cypress io examples page', () => {
+      cy.visit('https://example.cypress.io')
+      cy.url().should('eq', 'https://example.cypress.io/')
+    })
   
-
-})
+    
+  
+  })
